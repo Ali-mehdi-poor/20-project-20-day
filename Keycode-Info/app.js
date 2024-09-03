e614fe9225dbb6c856bf69f5eff6622e25e580ad
wolferina <73065645+wolferina@users.noreply.github.com>
2024-09-03 16:00:10 +0330

    Complete js Coding.

--- Keycode-Info/app.js
+++ Keycode-Info/app.js
@@ -6,3 +6,27 @@ let locationKey = $.getElementById("location");
 let keyElem = $.getElementById("key");
 let whichElem = $.getElementById("which");
 let codeElem = $.getElementById("code");
+
+
+document.body.addEventListener("keydown", function (event) {
+    event.preventDefault();
+    
+    starter.style.display = "none";
+    heading.style.display = "flex";
+    ascii.style.display = "flex";
+    infos.style.display = "flex";
+  
+    let eventKeyCode = event.keyCode;
+    let eventLocation = event.location;
+    let eventKey = event.key;
+    let eventWhich = event.which;
+    let eventCode = event.code;
+  
+    keyCodeElem.innerHTML = eventKeyCode;
+    locationKey.innerHTML = eventLocation;
+    keyElem.innerHTML = eventKey;
+    whichElem.innerHTML = eventWhich;
+    codeElem.innerHTML = eventCode;
+  
+});
+  
