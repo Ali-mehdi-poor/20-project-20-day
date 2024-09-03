bb0a3b86fc0c121f0f48062349b780dfd1d3bf1f
wolferina <73065645+wolferina@users.noreply.github.com>
2024-09-03 15:58:12 +0330

    select all elements that i need them

---
+++ Keycode-Info/app.js
@@ -0,0 +1,8 @@
+let $ = document;
+
+let title = $.querySelector("title");
+let keyCodeElem = $.getElementById("keyCode");
+let locationKey = $.getElementById("location");
+let keyElem = $.getElementById("key");
+let whichElem = $.getElementById("which");
+let codeElem = $.getElementById("code");
